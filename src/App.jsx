549a607ccc7a52ce918929b872f6be2d682db0af
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-05-02 21:26:05 +0530

    sdf

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useSelector } from 'react-redux'
+import './App.css'
+import Login from './components/Login'
+import Products from './components/Products'
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import { showCart } from './slices/cartSlice'
+
+
+function App() {
+
+  let auth = useSelector(state=> state.auth.auth)
+  let cart = useSelector(state=> state.cart.cartList)
+  let showCart = useSelector(state=> state.cart.showCart)
+ 
+
+  console.log(cart)
+  console.log(auth)
+
+ return (
+
+    <>
+    
+    { auth &&  <Navbar/>}
+    { !auth &&  <Login/>}
+    { auth &&  <Products/>}
+
+{ showCart && <Cart/>}
+    
+
+       
+        
+    </>
+  )
+}
+
+export default App
